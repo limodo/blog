327b905ffc60eb0b3216246f5bce6dec31a26cda
limodo <limodo@sina.cn>
2014-07-21 07:16:35 +0800

    更新已发布的Drupal资源文档，通过js让链接在新窗口打开

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-	$('#headline').html('<i class="icon-home"></i><a href="../index.html">首页</a>');
-	$('#content').append('<hr/><a href="../page/liuyan.html" target="_blank"><button class="btn"><i class="icon-comment"></i> 我有话要说！</button></a>');
+	$('#headline').html('<i class="icon-home"></i><a href="../index.html" target="_self">首页</a>');
+	$('#content').append('<hr/><a href="../page/liuyan.html"><button class="btn"><i class="icon-comment"></i> 我有话要说！</button></a>');
+	$('title').after('<base target="_blank" />');
 });
