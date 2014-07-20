d4791287d75c17ffa0cb3df001d0cc41b8944e60
limodo <limodo@sina.cn>
2014-07-21 00:55:38 +0800

    添加介绍drupal资源的一些文档（其他文件也略作修改）

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,1 +1,4 @@
-$(document).ready(function(){$('#headline').html('<i class="icon-home"></i><a href="../index.html">首页</a>');});
+$(document).ready(function(){
+	$('#headline').html('<i class="icon-home"></i><a href="../index.html">首页</a>');
+	$('#content').append('<hr/><button class="btn"><a href="../page/liuyan.html" target="_blank"><i class="icon-comment"></i> 我有话要说！</a></button>');
+});
