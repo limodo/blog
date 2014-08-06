00ac45d6972b3531843be81337015fbb393b96de
limodo <limodo@sina.cn>
2014-08-07 01:56:38 +0800

    修改留言按钮

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$('#headline').html('<i class="icon-home"></i><a href="../index.html" target="_self">首页</a>');
-	$('#content').append('<hr/><a href="../page/liuyan.html"><button class="btn btn-warning"><i class="icon-comment"></i> 我有话要说！</button></a>');
+	$('#content').append('<hr/><a href="../page/liuyan.html"><button class="btn btn-warning"><i class="icon-comment icon-white"></i> 我有话要说！</button></a>');
 	$('title').after('<base target="_blank" />');
 });
